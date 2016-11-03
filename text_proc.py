70d9539a4f32dbe6c1d0d08d83e312ba3cce6802
João Pereira <pereiraffjoao1993@gmail.com>
2016-11-03 12:17:17 +0000

    first commit; 1st, 2nd and 3rd tutorial uploaded; missing tutorial 4th, 5th, 6th and 7h

---
+++ text_proc.py
@@ -0,0 +1,31 @@
+from __future__ import division
+def lexical_diversity(text):
+    return len(text) / len(set(text))
+
+def lexical_diversity(my_text_data):
+    word_count = len(my_text_data)
+    vocab_size = len(set(my_text_data))
+    diversity_score = vocab_size / word_count
+    return diversity_score
+
+def plural(word):
+    if word.endswith('y'):
+        return word[:-1] + 'ies'
+    elif word[-1] in 'sx' or word[-2:] in ['sh', 'ch']:
+        return word + 'es'
+    elif word.endswith('an'):
+        return word[:-2] + 'en'
+    else:
+        return word + 's'
+
+import nltk
+def unusual_words(text):
+    text_vocab = set(w.lower() for w in text if w.isalpha())
+    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+    unusual = text_vocab - english_vocab
+    return sorted(unusual)
+
+def content_fraction(text):
+    stopwords = nltk.corpus.stopwords.words('english')
+    content = [w for w in text if w.lower() not in stopwords]
+    return len(content) / len(text)
